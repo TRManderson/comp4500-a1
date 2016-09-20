52c7260ef6d2f7fc8e73a8c73c2cc550a497f6dd
Tom Manderson <tom.manderson@polymathian.com>
2016-09-20 19:49:10 +1000

    Tests are now parameterised

--- src/assignment1/test/DelegateFinderTest.java
+++ src/assignment1/test/DelegateFinderTest.java
@@ -1,90 +1,123 @@
 package assignment1.test;
 
-import assignment1.*;
+import assignment1.Delegate;
+import assignment1.DelegateFinder;
+import assignment1.Event;
 import graphs.DGraphAdj;
 import graphs.Vertex;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.util.*;
+import java.util.function.Supplier;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 /**
- * Basic tests for the {@link DelegateFinder} implementation class.
- * 
- * We will use a more comprehensive test suite to test your code, so you should
- * add your own tests to this test suite to help you to debug your
- * implementation.
+ * Created by trm on 20/09/16.
  */
+@RunWith(Parameterized.class)
 public class DelegateFinderTest {
+    private TestEntry fixture;
 
-	/** A basic test based on the example from the assignment handout **/
-	@Test
-	public void basicTest() {
+    public DelegateFinderTest(Supplier<TestEntry> fixtureGenerator){
+        fixture = fixtureGenerator.get();
+    }
 
-		// the delegates who will take part in the summit
-		Delegate[] delegates = { new Delegate("d0"), new Delegate("d1"),
-				new Delegate("d2"), new Delegate("d3") };
+    private static class TestEntry{
+        private Set<Delegate> delegates;
+        private DGraphAdj<Vertex, Event> graph;
+        private Vertex start;
+        private Vertex end;
+        private Set<Delegate> initial;
+        private Set<Delegate> result;
 
-		// create the events in the summit
-		Delegate[][][] eventArray = {
-				{ { delegates[0] }, { delegates[1] }, { delegates[2] },
-						{ delegates[3] } },
+        public TestEntry(Set<Delegate> delegates,
+                         DGraphAdj<Vertex, Event> graph, Vertex start, Vertex end,
+                         Set<Delegate> initial, Set<Delegate> result){
+            this.delegates = delegates;
+            this.graph = graph;
+            this.start = start;
+            this.end = end;
+            this.initial = initial;
+            this.result = result;
+        }
+    }
 
-				{ {}, { delegates[0] }, { delegates[1], delegates[2] },
-						{ delegates[3] } },
+    @Parameterized.Parameters
+    public static Iterable<Supplier<TestEntry>> testEntry() {
+        List<Supplier<TestEntry>> data = new LinkedList<>();
+        data.add(() -> {
+            // the delegates who will take part in the summit
+            Delegate[] delegates = { new Delegate("d0"), new Delegate("d1"),
+                    new Delegate("d2"), new Delegate("d3") };
 
-				{ { delegates[0] }, { delegates[1] }, { delegates[2] },
-						{ delegates[3] } }, };
+            // create the events in the summit
+            Delegate[][][] eventArray = {
+                    { { delegates[0] }, { delegates[1] }, { delegates[2] },
+                            { delegates[3] } },
 
-		Event[] events = { createEvent(delegates, eventArray[0]),
-				createEvent(delegates, eventArray[1]),
-				createEvent(delegates, eventArray[2]) };
+                    { {}, { delegates[0] }, { delegates[1], delegates[2] },
+                            { delegates[3] } },
 
-		// the graph describing the possible proceedings of the summit
-		DGraphAdj<Vertex, Event> graph = new DGraphAdj<>();
-		Vertex[] vertices = { new Vertex(), new Vertex(), new Vertex() };
-		for (Vertex v : vertices) {
-			graph.addVertex(v);
-		}
-		graph.addEdge(vertices[0], vertices[1], events[0]);
-		graph.addEdge(vertices[1], vertices[2], events[1]);
-		graph.addEdge(vertices[2], vertices[1], events[2]);
+                    { { delegates[0] }, { delegates[1] }, { delegates[2] },
+                            { delegates[3] } }, };
 
-		// the start and end vertices of the proceedings graph
-		Vertex start = vertices[0];
-		Vertex end = vertices[2];
+            Event[] events = { createEvent(delegates, eventArray[0]),
+                    createEvent(delegates, eventArray[1]),
+                    createEvent(delegates, eventArray[2]) };
 
-		// the delegates in favour of the decision at the start of the summit
-		Set<Delegate> initial = new HashSet<>();
-		initial.add(delegates[0]);
+            // the graph describing the possible proceedings of the summit
+            DGraphAdj<Vertex, Event> graph = new DGraphAdj<>();
+            Vertex[] vertices = { new Vertex(), new Vertex(), new Vertex() };
+            for (Vertex v : vertices) {
+                graph.addVertex(v);
+            }
+            graph.addEdge(vertices[0], vertices[1], events[0]);
+            graph.addEdge(vertices[1], vertices[2], events[1]);
+            graph.addEdge(vertices[2], vertices[1], events[2]);
 
-		// the expected result of the test
-		Set<Delegate> expectedResult = new HashSet<>();
-		expectedResult.add(delegates[1]);
-		expectedResult.add(delegates[2]);
+            // the start and end vertices of the proceedings graph
+            Vertex start = vertices[0];
+            Vertex end = vertices[2];
 
-		// the actual result of the test
-		Set<Delegate> actualResult = DelegateFinder.findDelegates(
-				new HashSet<Delegate>(Arrays.asList(delegates)), graph, start,
-				end, initial);
+            // the delegates in favour of the decision at the start of the summit
+            Set<Delegate> initial = new HashSet<>();
+            initial.add(delegates[0]);
 
-		Assert.assertEquals(expectedResult, actualResult);
-	}
+            // the expected result of the test
+            Set<Delegate> expectedResult = new HashSet<>();
+            expectedResult.add(delegates[1]);
+            expectedResult.add(delegates[2]);
 
-	/**
-	 * A helper function for creating an event from an array of the delegates
-	 * who will take part in the event, and an array of arrays describing the
-	 * corresponding dependencies for each delegate.
-	 */
-	private static Event createEvent(Delegate[] delegatesArray,
-			Delegate[][] dependentArray) {
-		Map<Delegate, Set<Delegate>> event = new HashMap<>();
-		for (int i = 0; i < delegatesArray.length; i++) {
-			event.put(delegatesArray[i],
-					new HashSet<Delegate>(Arrays.asList(dependentArray[i])));
-		}
-		return new Event(event);
-	}
+            Set<Delegate> delegateSet = new HashSet<Delegate>();
+            delegateSet.addAll(Arrays.asList(delegates));
+            return new TestEntry(delegateSet, graph, start, end, initial, expectedResult);
+        });
 
+        return data;
+    }
+
+    @Test
+    public void testExpectedResult(){
+        Set<Delegate> result = DelegateFinder.findDelegates(
+                fixture.delegates, fixture.graph,
+                fixture.start, fixture.end,
+                fixture.initial);
+        Assert.assertEquals(fixture.result, result);
+    }
+
+    public static Event createEvent(Delegate[] delegatesArray,
+                                    Delegate[][] dependentArray) {
+        Map<Delegate, Set<Delegate>> event = new HashMap<>();
+        for (int i = 0; i < delegatesArray.length; i++) {
+            event.put(delegatesArray[i],
+                    new HashSet<Delegate>(Arrays.asList(dependentArray[i])));
+        }
+        return new Event(event);
+    }
 }
